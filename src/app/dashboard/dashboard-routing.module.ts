46763fbca5dbb42b284b4b1da5271f9ee77e8e91
Ivan Sousa <jorge.almada@adroitoverseas.net>
2021-12-01 13:30:19 -0700

    update.

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
-import { ListadoComponent } from './listado/listado.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
@@ -9,7 +8,6 @@ const routes: Routes = [
     path:'', 
     component: DashboardComponent,
     children:[
-      {path: '', component: ListadoComponent},
       {path: 'register', component: RegisterComponent},
       {path: '**', redirectTo: ''}
     ]
